fb6732db0e6b27763cc7a854f288bfb3c60c674c
koa73 <Ghtlfnjh12>
2016-08-10 16:41:35 +0300

    10.08.2016/v2

--- src/main/java/Reseiver.java
+++ src/main/java/Reseiver.java
@@ -16,19 +16,38 @@ public class Reseiver {
             java.lang.InterruptedException {
 
         Connection connection = new ConnectionQueue().getConnection();
-        Channel channel = connection.createChannel();
+        final Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(QUEUE_NAME, true, false, true, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // maximum number of messages that the server will deliver, 0 if unlimited
+        channel.basicQos(1);
 
         Consumer consumer = new DefaultConsumer(channel) {
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
                     throws IOException {
                 String message = new String(body, "UTF-8");
-                System.out.println(" [x] Received '" + message + "'");
+                try{
+
+                    System.out.println(" [x] Received '" + message + "'");
+                    doWork();
+
+                } finally {
+                    System.out.println(" [x] Done ");
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
             }
         };
-        channel.basicConsume(QUEUE_NAME, true, consumer);
+        channel.basicConsume(QUEUE_NAME, false, consumer);
+    }
+
+    private static void doWork() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException _ignored) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -4,9 +4,9 @@
  */
 
 import Rabbit.ConnectionQueue;
-import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 
 
 public class Sender {
@@ -18,10 +18,16 @@ public class Sender {
 
             Connection connection = new ConnectionQueue().getConnection();
             Channel channel = connection.createChannel();
-
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+/*
+queue - the name of the queue
+durable - true if we are declaring a durable queue (the queue will survive a server restart)
+exclusive - true if we are declaring an exclusive queue (restricted to this connection)
+autoDelete - true if we are declaring an autodelete queue (server will delete it when no longer in use)
+arguments - other properties (construction arguments) for the queue
+ */
+            channel.queueDeclare(QUEUE_NAME, true, false, true, null);
             String message = "Hello World!";
-            channel.basicPublish("", QUEUE_NAME, null,
+            channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN,
                     message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + message + "'");
 
